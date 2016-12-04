3f4994f5fc3ccb2190271881be324c770bf8e459
DESKTOP-6JBMRUJ\Huzdu <huzdup.alexandros@yahoo.com>
2016-12-04 22:55:11 +0200

    A code which delete white spaces and revert a number in a string with a digit.

---
+++ Jaava 2016/StringUtilityTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilityTest {
+
+	@Test
+	public void test1() {
+		// given
+		String string = new String("i have four apples.");
+		StringUtility b = new StringUtility();
+		// when
+		String newString = b.replaceNameOfNumbersWithDigits(string);
+		String newString2 = b.replaceWhiteSpaces(newString);
+		// then
+		String correctString = new String("ihave4apples.");
+		Assert.assertEquals(correctString, newString2);
+	}
+	
+	@Test
+	public void test2() {
+		// given
+		String string = new String(" ONe TWO THREE fouRty");
+		StringUtility b = new StringUtility();
+		// when
+		String newString = b.replaceNameOfNumbersWithDigits(string);
+		String newString2 = b.replaceWhiteSpaces(newString);
+		// then
+		String correctString = new String("1234ty");
+		Assert.assertEquals(correctString, newString2);
+	}
+
+}

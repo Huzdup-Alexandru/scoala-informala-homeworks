c34ba55ba2d0f6ece42715236f3ca5734d6425ec
DESKTOP-6JBMRUJ\Huzdu <huzdup.alexandros@yahoo.com>
2016-12-07 22:45:23 +0200

    no message

--- Java 2016/Homework week 6/StringUtilityTest.java
+++ Java 2016/Homework week 6/StringUtilityTest.java
@@ -30,5 +30,32 @@ public class StringUtilityTest {
 		String correctString = new String("1234ty");
 		Assert.assertEquals(correctString, newString2);
 	}
+    
+	@Test
+	public void test3() {
+		// given
+		String string = new String("i have nInE Oranges in my bag.");
+		StringUtility b = new StringUtility();
+		// when
+		String newString = b.replaceNameOfNumbersWithDigits(string);
+		String newString2 = b.replaceWhiteSpaces(newString);
+		// then
+		String correctString = new String("ihave9Orangesinmybag.");
+		Assert.assertEquals(correctString, newString2);
+	}
+
+	@Test
+	public void test4() {
+		// given
+		String string = new String("nineteen years ago       .");
+		StringUtility b = new StringUtility();
+		// when
+		String newString = b.replaceNameOfNumbersWithDigits(string);
+		String newString2 = b.replaceWhiteSpaces(newString);
+		// then
+		String correctString = new String("nineteenyearsago.");
+		Assert.assertEquals(correctString, newString2);
+	}
+}
 
 }
